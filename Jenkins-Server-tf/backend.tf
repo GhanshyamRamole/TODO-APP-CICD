032312c13b9383b0918e3071e3cf28c94aed2b26
Ubuntu <ubuntu@ip-172-31-16-54.us-west-1.compute.internal>
2026-01-17 06:27:12 +0000

    backend remote terraform.tfstate For centralized Terraform usage

---
+++ Jenkins-Server-tf/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket       = "jenkins-bucket" 
+    region       = "us-east-1"
+    key          = "DevOps/jenkins-server/terraform.tfstate"
+    encrypt      = true
+    
+    # updated WAY configure backend:
+    use_lockfile = true             
+    # dynamodb_table = "Lock-Files"  
+  }
+
+  required_version = ">= 1.10.0"    
+  
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
